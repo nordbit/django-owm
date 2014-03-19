8281f9c1d99e89d5de02cab868b797cdcbb96382
Mirza Delic <mirzadelic1@gmail.com>
2014-03-19 20:30:12 +0100

    try, except

--- owm/templatetags/owm_tags.py
+++ owm/templatetags/owm_tags.py
@@ -3,6 +3,7 @@ from owm.models import Weather
 
 register = template.Library()
 
+
 def get_weather(context, city, country):
     return {
         'weather': Weather.get_values(city, country),
@@ -12,6 +13,10 @@ def get_weather(context, city, country):
 # Register the custom tag as an inclusion tag with takes_context=True.
 register.inclusion_tag('owm/weather.html', takes_context=True)(get_weather)
 
+
 @register.filter
 def to_int(value):
-    return int(value)
+    try:
+        return int(value)
+    except:
+        pass
