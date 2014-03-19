36b1268b400d13f4be45c83bbccbea4efe21dd25
Enver Bisevac <enver@bisevac.com>
2014-03-19 09:18:19 +0100

    Small changes in tags

--- owm/templatetags/owm_tags.py
+++ owm/templatetags/owm_tags.py
@@ -6,6 +6,8 @@ register = template.Library()
 def get_weather(context, city, country):
     return {
         'weather': Weather.get_values(city, country),
+        'city': city,
+        'country': country,
     }
 # Register the custom tag as an inclusion tag with takes_context=True.
 register.inclusion_tag('owm/weather.html', takes_context=True)(get_weather)
